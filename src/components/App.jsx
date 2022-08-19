48002c2dc03fc6733984b7e3a2be30b9cb31d21e
ArtemYakushkin <zimberger29101985@gmail.com>
2022-08-19 22:45:07 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+const { Component } = require("react");
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+export default App;
+
+// 'https://pixabay.com/api/?q=cat&page=1&key=28419323-07717efaf215650b1b40134d5&image_type=photo&orientation=horizontal&per_page=12'
